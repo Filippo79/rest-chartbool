1487e258f794f0852c14df56fdb0c84e660228ae
Filippo Pittau <filippo.pittau@gmail.com>
2020-04-08 16:56:24 +0200

    Grafico 1

--- js/main.js
+++ js/main.js
@@ -4,29 +4,70 @@ $(document).ready(function() {
         method:'GET',
         success: function(data) {
             //console.log(data);
-            var datiClienti = data;
-            for (var i = 0; i < datiClienti.length; i++) {
-                var datiCliente = datiClienti[i];
+            var venditePerMese = {
+                    'gennaio': 0,
+                    'febbraio': 0,
+                    'marzo': 0,
+                    'aprile': 0,
+                    'maggio': 0,
+                    'giugno': 0,
+                    'luglio': 0,
+                    'agosto': 0,
+                    'settembre': 0,
+                    'ottobre': 0,
+                    'novembre': 0,
+                    'dicembre': 0
+            };
+            var dati = data;
+            for (var i = 0; i < dati.length; i++) {
+                var datiCliente = dati[i];
                 //console.log(datiClienti[i]);
-                var nomeCliente = datiCliente.salesman;
-                var vendutoCliente = datiCliente.amount;
+                var nomiClienti = datiCliente.salesman;
+                var vendutoClienti = datiCliente.amount;
                 var dataVendita = datiCliente.date;
+                var mesiVendita = moment(dataVendita, 'DD/MM/YYYY').format("MMMM");
+                // console.log(mesiVendita);
+                if ( vendutoClienti === undefined){
+                    vendutoClienti = 0;
+                }
+                // console.log(vendutoClienti);
+                venditePerMese[mesiVendita] += vendutoClienti;
+                // console.log(venditePerMese);
             }
-            // console.log(vendutoCliente);
-            // console.log(dataVendita);
-            // console.log(nomeCliente);
+
+            var venduto =[];
+            for (var key in venditePerMese) {
+                // console.log(key);
+                venduto.push(venditePerMese[key]);
+            } console.log(venduto);
+            return venduto;
+
+
+
+
+
         }
     });
+    // var ctx = $('#ChartBool');
+    // var chart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: venduto,
+    //     options: options
+    // });
 
-
-
-
-    /*var ctx = $('#ChartBool');
+    var ctx = $('#ChartBool');
     var chart = new Chart(ctx, {
         type: 'line',
-        data: data,
-        options: options
-    });*/
+        data: {
+            labels: ['Gennaio' , 'Febbraio' , 'Marzo' , 'Aprile' , 'Maggio' , 'Giugno' ,'Luglio' ,'Agosto' , 'Settembre' , 'Ottobre' , 'Novembre' , 'Dicembre'],
+            datasets: [{
+                label: 'Vendite Annuali',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: venduto,
+            }],
+        },
+    });
 
 
 });
